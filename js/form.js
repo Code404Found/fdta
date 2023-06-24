42ef4f279858f38d1b84e23b1695496b86ff6629
Anya Fleming <128553712+amfleming1@users.noreply.github.com>
2023-06-24 15:01:07 +0000

    added event handler

--- js/form.js
+++ js/form.js
@@ -8,13 +8,3 @@ placeholderSupport = placeholderIsSupported() ? 'placeholder' : 'no-placeholder'
 $('html').addClass(placeholderSupport);  
 });
 
-<html>
-    <body>
-        <input type="submit" onClick="myFunction()"/>
-        <script> 
-            function myFunction() {
-                window.location.href="img/ticket.png"
-            }
-        </script>
-    </body>
-</html>
